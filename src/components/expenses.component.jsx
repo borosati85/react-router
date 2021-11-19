1a5d1b1c56910d4f73b53734190099f875167a2a
Attila <borosati85@gmail.com>
2021-11-19 18:36:23 +0100

    initial commit

---
+++ src/components/expenses.component.jsx
@@ -0,0 +1,6 @@
+const Expenses = () =>
+    <main style={{ padding: "1rem 0" }}>
+        <h2>Expenses</h2>
+    </main>
+
+export default Expenses;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Expenses from './components/expenses.component';
+import Invoices from './components/invoices.component';
+import Invoice from './components/invoice.component';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route path='/expenses' element={<Expenses />}/>
+          <Route path='/invoices' element={<Invoices />}>
+            <Route
+              index
+              element={
+                <main style={{ padding: "1rem" }}>
+                  <p>Select an invoice</p>
+                </main>
+              }
+            />
+            <Route path=':invoiceId' element={<Invoice />} />
+          </Route>
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
+        </Route>
+      </Routes>    
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
